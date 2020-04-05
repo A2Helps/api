9d119cbb77832824efa49077634e5ca1bc178b89
Patrick McCarren <patrick@cumulative.io>
2020-04-05 12:07:30 -0400

    copy: wire instructions

--- resources/views/mail/wire-instructions/text.blade.php
+++ resources/views/mail/wire-instructions/text.blade.php
@@ -1,4 +1,12 @@
-Bank: Key Bank
-Routing number: 041001039
-Account number: 229081009172
-Memo: A2 Helps
+Thank you for expressing interest in donating to A2 Helps. Your generosity will help local businesses and front-lines healthcare professionals during the COVID-19 crisis. The following is what we need from you to complete the ACH Wire Transfer.
+
+    Bank: Key Bank
+    Routing number: 041001039
+    Account number: 229081009172
+    Memo: A2 Helps
+
+Thank you again for your generosity and compassion for those serving us in Washtenaw County.
+
+Sincerely,
+
+The A2 Helps Team
